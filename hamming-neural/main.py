d3ac74b4a3f7b78c0dfbf761ccc04c4754d6d49b
Mikhail Ivanov <mivanov@mirantis.com>
2015-10-27 20:11:40 +0300

    Added coder, work on decoder.
    For now supports only english charset

--- hamming-neural/main.py
+++ hamming-neural/main.py
@@ -3,51 +3,6 @@
 
 __author__ = 'michael'
 
-
-def selection(list21, start):
-    for i in range(start, len(list21)/2):
-        print list21[2*i]
-    print '==========='
-
-
-
-def code(input):
-    break_word = True
-    i = 0
-    indexes = []
-    array = list(input)
-    while break_word is not False:
-        array.insert(2**i-1, '0')
-        indexes.append(i)
-        if 2**(i+1) > len(array):
-            break_word = False
-        i += 1
-
-    print indexes
-
-    #for i in indexes:
-    selection(array, 2**2-1)
-    #print '=========================='
-    #for i in range(0, len(array), 2):
-    #    print i
-    #print '=========================='
-    #for i in range(1, len(array), 4):
-    #    print i, i+1
-    #print '=========================='
-    #for i in range(3, len(array), 8):
-    #    print i, i+1, i+2, i+3
-    #print '=========================='
-
-    return array, indexes
-    #ham_code =
-    #return ham_code
-
-a = 'd'
-b = 'D'
-a = ''.join(format(ord(x), 'b') for x in a)
-print ''.join(code(a)[0])
-#print ''.join(format(ord(x), 'b') for x in b)
-
 # Основываем на нечетности - если количество контролируемых бит, равных единице нечетно, инвертируем контрольный бит.
 # Изначально записываем нули на места степени двойки
 # +------------+-------------------+
@@ -59,3 +14,69 @@ print ''.join(code(a)[0])
 # +------------+-------------------+
 # |count_1     | 11111001100       |
 # +------------+-------------------+
+
+
+def selection(list21, cond):
+    """
+    Функция для выборки контролируемых бит, и подсчета четности/нечетности для инвертирования контрольного бита
+    :param list21: Массив, хранящий в себе начальное состояние до конвертации
+    :param cond: Параметр, определяющий начальную позицию и шаг выборки
+    :return: флаг, говорящий о необходимости конвертации. Равен True, если битов == 1 нечетное количество,
+    и False, если битов == 1 четное количество
+    """
+    # Счетчик единиц в контролируемых битах
+    counter = 0
+    for i in range(cond-1, len(list21), cond*2):
+        try:
+            counter += sum(list21[i:i+cond])
+        except IndexError:
+            pass
+    if counter % 2 == 1:
+        flag = True
+    else:
+        flag = False
+    print '==========='
+    return flag
+
+
+def code(input):
+    break_word = True
+    i = 0
+    indexes = []
+    array = map(int, list(input))
+    while break_word is not False:
+        array.insert(2**i-1, 0)
+        indexes.append(i)
+        if 2**(i+1) > len(array):
+            break_word = False
+        i += 1
+
+    print indexes
+
+    for i in indexes:
+        flag = selection(array, 2**i)
+        if flag:
+            array[2**i-1] = 1
+    print '\n=========================='
+
+    ham_code = array
+    return ham_code
+
+
+def decode():
+    pass
+
+
+#a = 'ы'
+#b = 'D'
+#a = ''.join(format(ord(x), 'b') for x in a)
+#print a
+#print ''.join(str(i) for i in code(a))
+# After running code, d = 11111001100
+print chr(int('1101000110001011', 2)) #'11111001100', 2))
+# Wrong d = 11011001100
+#a = '11011001100'
+#print map(int, list(a))
+
+#print ''.join(format(ord(x), 'b') for x in b)
+
